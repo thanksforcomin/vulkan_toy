6bb4e38888e1c25b9fbb9a1b5e3a622bc005006f
andrew <dmtr.bhn@yandex.ru>
2024-07-11 21:31:43 +0300

    extensions

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
-#include "include/vulkan/vulkan_utils.hpp"
-#include "include/vulkan/vulkan_initializers.hpp"
+#include "include/core/context.hpp"
+
+#include <iostream>
 
 int main() {
+  std::vector<const char*> instance_extensions{VK_KHR_GET_PHYSICAL_DEVICE_PROPERTIES_2_EXTENSION_NAME};
+  std::vector<const char*> device_extensions{VK_KHR_DYNAMIC_RENDERING_EXTENSION_NAME, 
+                                              VK_KHR_MAINTENANCE2_EXTENSION_NAME,
+                                              VK_KHR_MULTIVIEW_EXTENSION_NAME,
+                                              VK_KHR_CREATE_RENDERPASS_2_EXTENSION_NAME,
+                                              VK_KHR_DEPTH_STENCIL_RESOLVE_EXTENSION_NAME,
+                                              VK_KHR_SWAPCHAIN_EXTENSION_NAME,
+                                              VK_KHR_COPY_COMMANDS_2_EXTENSION_NAME};
+
+  core::vulkan_context context(instance_extensions, device_extensions);
+  std::cout << "wrapping up" << std::endl;
+
   return 0;
 }
